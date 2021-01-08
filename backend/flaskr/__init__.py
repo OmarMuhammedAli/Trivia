846bbd9492289bcc8bc7614c83f45ee70f7e69b5
Omar <omarmuhammed1998.om@gmail.com>
2021-01-08 23:02:49 +0200

    Added paginate_questions helper method

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -9,6 +9,19 @@ from ..models import setup_db, Question, Category
 QUESTIONS_PER_PAGE = 10
 
 
+def paginate_questions(request, questions):
+    """
+    A helper method to return questions paginated
+    """
+    page = request.args.get('page', 1, type=int)
+    start = (page - 1) * QUESTIONS_PER_PAGE
+    end = start + QUESTIONS_PER_PAGE
+    current_questions = [question.format()
+                         for question in questions][start:end]
+
+    return current_questions
+
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
