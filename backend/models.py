273732a5131750af1fe1880d1ac7bbc523d6962e
Omar <omarmuhammed1998.om@gmail.com>
2021-01-09 02:04:53 +0200

    added migrations and modified the models

--- backend/models.py
+++ backend/models.py
@@ -1,23 +1,33 @@
 import os
 from sqlalchemy import Column, String, Integer, create_engine
+from sqlalchemy.engine.url import URL
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import json
 
-database_name = "trivia"
-database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+DB_URI = {
+    'drivername': 'postgres',
+    'username': 'horizon',
+    'password': '0105415595',
+    'host': 'localhost',
+    'port': '5432',
+    'database': 'trivia'
+}
+
 
 db = SQLAlchemy()
-
+migrate = Migrate()
 '''
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
-def setup_db(app, database_path=database_path):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+def setup_db(app, database_path=DB_URI):
+    app.config["SQLALCHEMY_DATABASE_URI"] = URL(**database_path)
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    db.create_all()
+    migrate.init_app(app, db)
+    
 
 '''
 Question
