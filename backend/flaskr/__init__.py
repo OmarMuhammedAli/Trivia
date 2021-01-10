ff7cfef838a266585475b06d1fde89e25cfa5280
Omar <omarmuhammed1998.om@gmail.com>
2021-01-10 23:47:55 +0200

    Added endpoint for retrieval of questions, paginated

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -44,26 +44,29 @@ def create_app(test_config=None):
         return response
 
     '''
-  @TODO: 
+  @TODO-: 
   Create an endpoint to handle GET requests 
-  for all available categories.
+  for all available categories.(Done!)
   '''
     @app.route('/categories')
     def retrieve_categories():
-        categories = Category.query.all()
+        try:
+            categories = Category.query.order_by(Category.id).all()
 
-        if len(categories) < 1:
-            abort(404)
+            if len(categories) < 1:
+                abort(404)
 
-        formatted_categories = [category.format() for category in categories]
-        return jsonify({
-            'success': True,
-            'categories': formatted_categories,
-            'total_categories': len(categories)
-        })
+            formatted_categories = [category.format() for category in categories]
+            return jsonify({
+                'success': True,
+                'categories': formatted_categories,
+                'total_categories': len(categories)
+            })
+        except: 
+            abort(500)
 
     '''
-  @TODO: 
+  @TODO-: 
   Create an endpoint to handle GET requests for questions, 
   including pagination (every 10 questions). 
   This endpoint should return a list of questions, 
@@ -72,8 +75,29 @@ def create_app(test_config=None):
   TEST: At this point, when you start the application
   you should see questions and categories generated,
   ten questions per page and pagination at the bottom of the screen for three pages.
-  Clicking on the page numbers should update the questions. 
+  Clicking on the page numbers should update the questions. (Done!)
   '''
+    @app.route('/questions')
+    def retrieve_paginated_questions():
+
+        try: 
+            questions = Question.query.order_by(Question.id).all()
+            if len(questions) < 1: abort(404)
+            paginated_questions = paginate_questions(request, questions)
+            # print(paginated_questions)
+            
+            categories = Category.query.order_by(Category.id).all()
+            formatted_categories = [category.format() for category in categories]
+
+            return jsonify({
+                'success': True,
+                'questions': paginated_questions,
+                'total_questions': len(questions),
+                'current_category': None,
+                'categories': formatted_categories
+            }), 200
+        except:
+            abort(500)
 
     '''
   @TODO: 
