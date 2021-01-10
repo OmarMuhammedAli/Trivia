befaba9b0539a9e3372589a6f5b37f41ca9867b4
Omar <omarmuhammed1998.om@gmail.com>
2021-01-11 00:38:11 +0200

    corrected flag "exists" position

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -109,11 +109,11 @@ def create_app(test_config=None):
             abort(500)
 
     '''
-  @TODO: 
+  @TODO-: 
   Create an endpoint to DELETE question using a question ID. 
 
   TEST: When you click the trash icon next to a question, the question will be removed.
-  This removal will persist in the database and when you refresh the page. 
+  This removal will persist in the database and when you refresh the page. (Done!)
   '''
     @app.route('/questions/<int:question_id>', methods=['DELETE'])
     def delete_question(question_id):
@@ -121,8 +121,9 @@ def create_app(test_config=None):
         try: 
             question = Question.query.get(question_id)
             if question is None: 
-                abort(404)
                 exists = False
+                abort(404)
+                
             question.delete()
         except: 
             if not exists: abort(404)
