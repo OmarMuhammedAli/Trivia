5b4fb9cf0acfd9190bd1fda17c42cfa8bcad234e
Omar <omarmuhammed1998.om@gmail.com>
2021-01-11 02:41:50 +0200

    Added the functionality to rerfuse addition of already existing questions

--- frontend/src/components/FormView.js
+++ frontend/src/components/FormView.js
@@ -1,51 +1,50 @@
-import React, { Component } from 'react';
-import $ from 'jquery';
+import React, { Component } from "react";
+import $ from "jquery";
 
-import '../stylesheets/FormView.css';
+import "../stylesheets/FormView.css";
 
 class FormView extends Component {
-  constructor(props){
+  constructor(props) {
     super();
     this.state = {
       question: "",
       answer: "",
       difficulty: 1,
       category: 1,
-      categories: {}
-    }
+      categories: {},
+    };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     $.ajax({
       url: `/categories`, //TODO: update request URL
       type: "GET",
       success: (result) => {
-        this.setState({ categories: result.categories })
+        this.setState({ categories: result.categories });
         return;
       },
       error: (error) => {
-        alert('Unable to load categories. Please try your request again')
+        alert("Unable to load categories. Please try your request again");
         return;
-      }
-    })
+      },
+    });
   }
-
 
   submitQuestion = (event) => {
     event.preventDefault();
     $.ajax({
-      url: '/questions', //TODO: update request URL
+      url: "/questions", //TODO: update request URL
       type: "POST",
-      dataType: 'json',
-      contentType: 'application/json',
+      dataType: "json",
+      contentType: "application/json",
       data: JSON.stringify({
         question: this.state.question,
         answer: this.state.answer,
         difficulty: this.state.difficulty,
-        category: this.state.category
+        category: this.state.category,
       }),
       xhrFields: {
-        withCredentials: true
+        withCredentials: true,
       },
       crossDomain: true,
       success: (result) => {
@@ -53,28 +52,38 @@ class FormView extends Component {
         return;
       },
       error: (error) => {
-        alert('Unable to add question. Please try your request again')
+        if (error.status === 403) {
+            alert("Question already exists!")
+        }
+        else {
+            alert("Unable to add question. Please try your request again");
+        }
+        
         return;
-      }
-    })
-  }
+      },
+    });
+  };
 
   handleChange = (event) => {
-    this.setState({[event.target.name]: event.target.value})
-  }
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
   render() {
     return (
       <div id="add-form">
         <h2>Add a New Trivia Question</h2>
-        <form className="form-view" id="add-question-form" onSubmit={this.submitQuestion}>
+        <form
+          className="form-view"
+          id="add-question-form"
+          onSubmit={this.submitQuestion}
+        >
           <label>
             Question
-            <input type="text" name="question" onChange={this.handleChange}/>
+            <input type="text" name="question" onChange={this.handleChange} />
           </label>
           <label>
             Answer
-            <input type="text" name="answer" onChange={this.handleChange}/>
+            <input type="text" name="answer" onChange={this.handleChange} />
           </label>
           <label>
             Difficulty
@@ -89,11 +98,13 @@ class FormView extends Component {
           <label>
             Category
             <select name="category" onChange={this.handleChange}>
-              {Object.keys(this.state.categories).map(id => {
-                  return (
-                    <option key={id} value={id}>{this.state.categories[id]}</option>
-                  )
-                })}
+              {Object.keys(this.state.categories).map((id) => {
+                return (
+                  <option key={id} value={id}>
+                    {this.state.categories[id]}
+                  </option>
+                );
+              })}
             </select>
           </label>
           <input type="submit" className="button" value="Submit" />
